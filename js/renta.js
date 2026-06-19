6be313dcff367227e2ef4cf04cd110fe32b6d210
= <=>
2026-06-19 12:01:37 -0600

    Update renta.js

--- js/renta.js
+++ js/renta.js
@@ -232,10 +232,9 @@ $(document).ready(function() {
      */
     function disableForm() {
         const form = $('#bookingForm');
-        form.addClass('form-disabled');
+        form.addClass('form-disabled');               
         form.find('input, textarea').prop('readonly', true);
-        $("#guardar_frm").prop('readonly', false);
-        $("#guardar_frm").removeClass("form-disabled");
+        
         
     }
     
